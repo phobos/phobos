39ca40d7b64567b32fb7acd3af624c0a3692bf9f
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-08-10 21:13:41 +0200

    ensuring producer closes connection

--- lib/phobos/producer.rb
+++ lib/phobos/producer.rb
@@ -1,9 +1,12 @@
 module Phobos
   module Producer
+
     def publish(topic, payload, key = nil)
-      Phobos
-        .create_kafka_client
-        .deliver_message(payload, topic: topic, partition_key: key)
+      klafka_client = Phobos.create_kafka_client
+      klafka_client.deliver_message(payload, topic: topic, partition_key: key)
+    ensure
+      klafka_client&.close
     end
+
   end
 end
