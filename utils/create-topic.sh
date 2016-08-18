15b6ecad2e869cec8b869d6fe4985164b52296da
Sergey Evstifeev <sergey.evstifeev@klarna.com>
2016-08-18 16:11:35 +0200

    Replication factor must be higher than 0

--- utils/create-topic.sh
+++ utils/create-topic.sh
@@ -12,6 +12,6 @@ echo "creating topic ${TOPIC}, partitions ${PARTITIONS}"
 docker run --rm $KAFKA_IMAGE:$KAFKA_IMAGE_VERSION kafka-topics.sh \
   --create \
   --topic $TOPIC \
-  --replication-factor 0 \
+  --replication-factor 1 \
   --partitions $PARTITIONS \
   --zookeeper $ZK_IP:2181
