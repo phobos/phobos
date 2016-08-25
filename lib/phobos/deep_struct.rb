919e638c0effd0300df585c655faf55224973ce2
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-08-25 09:40:46 +0200

    Remove old code

--- lib/phobos/deep_struct.rb
+++ lib/phobos/deep_struct.rb
@@ -25,18 +25,5 @@ module Phobos
     def to_h
       @hash_table
     end
-
-    def self.extract(attrs)
-      keys = attrs.keys
-      Struct.new(*keys).new(*keys.map do |k|
-        if attrs[k].is_a? Hash
-          extract(attrs[k])
-        elsif attrs[k].is_a?(Array) && attrs[k].all? { |el| el.is_a?(Hash) }
-          attrs[k].map { |el| extract(el) }
-        else
-          attrs[k]
-        end
-      end)
-    end
   end
 end
