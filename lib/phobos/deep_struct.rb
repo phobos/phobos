a794d67c89481edb57677218d097297f447b9c1f
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-08-25 09:33:11 +0200

    Change DeepStruct to subclass OpenStruct instead

--- lib/phobos/deep_struct.rb
+++ lib/phobos/deep_struct.rb
@@ -1,5 +1,31 @@
+require 'ostruct'
 module Phobos
-  class DeepStruct
+  class DeepStruct < OpenStruct
+    def initialize(hash=nil)
+      @table = {}
+      @hash_table = {}
+
+      if hash
+        hash.each_pair do |k, v|
+          k = k.to_sym
+          @table[k] = if v.is_a?(Hash)
+            self.class.new(v)
+          elsif v.is_a?(Array) && v.all? { |el| el.is_a?(Hash) }
+            v.map { |el| self.class.new(el) }
+          else
+            v
+          end
+
+          @hash_table[k] = v
+          new_ostruct_member(k)
+        end
+      end
+    end
+
+    def to_h
+      @hash_table
+    end
+
     def self.extract(attrs)
       keys = attrs.keys
       Struct.new(*keys).new(*keys.map do |k|
--- spec/lib/phobos/deep_struct_spec.rb
+++ spec/lib/phobos/deep_struct_spec.rb
@@ -2,20 +2,28 @@ require 'spec_helper'
 
 RSpec.describe Phobos::DeepStruct do
   let(:example) { {a: 1, b: [2, 3], c: {x: 4, y: 5}, d: [{j: 6}, {j: 7}]} }
-  let(:subject) { described_class.extract(example) }
+  let(:subject) { described_class.new(example) }
 
-  it 'hash keys are accessible as method calls' do
-    expect(subject.a).to eq(1)
-    expect(subject.b).to eq([2, 3])
+  context '.new' do
+    it 'hash keys are accessible as method calls' do
+      expect(subject.a).to eq(1)
+      expect(subject.b).to eq([2, 3])
+    end
+
+    it 'deep hash keys are accessible as chained method calls' do
+      expect(subject.c.x).to eq(4)
+      expect(subject.c.y).to eq(5)
+    end
+
+    it 'handles array of hashes' do
+      expect(subject.d[0].j).to eq(6)
+      expect(subject.d[1].j).to eq(7)
+    end
   end
 
-  it 'deep hash keys are accessible as chained method calls' do
-    expect(subject.c.x).to eq(4)
-    expect(subject.c.y).to eq(5)
-  end
-
-  it 'handles array of hashes' do
-    expect(subject.d[0].j).to eq(6)
-    expect(subject.d[1].j).to eq(7)
+  context '#to_h' do
+    it 'renders itself as a hash' do
+      expect(subject.to_h).to eq(example)
+    end
   end
 end
