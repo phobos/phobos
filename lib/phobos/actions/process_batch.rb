ef07e820e84aeae0e8f8d497331d828eb77c1277
Jason Dougherty <jasondoc3@gmail.com>
2017-10-31 23:23:30 -0700

    Move retry logic into ProcessMessage

--- lib/phobos/actions/process_batch.rb
+++ lib/phobos/actions/process_batch.rb
@@ -11,7 +11,6 @@ module Phobos
 
       def execute
         @batch.messages.each do |message|
-          backoff = @listener.create_exponential_backoff
           metadata = @listener_metadata.merge(
             key: message.key,
             partition: message.partition,
@@ -19,40 +18,17 @@ module Phobos
             retry_count: 0
           )
 
-          begin
-            instrument('listener.process_message', metadata) do |metadata|
-              time_elapsed = measure do
-                Phobos::Actions::ProcessMessage.new(
-                  listener: @listener,
-                  message: message,
-                  metadata: metadata,
-                  encoding: @listener.encoding
-                ).execute
-              end
-              metadata.merge!(time_elapsed: time_elapsed)
-            end
-          rescue => e
-            retry_count = metadata[:retry_count]
-            interval = backoff.interval_at(retry_count).round(2)
-
-            error = {
-              waiting_time: interval,
-              exception_class: e.class.name,
-              exception_message: e.message,
-              backtrace: e.backtrace
-            }
-
-            instrument('listener.retry_handler_error', error.merge(metadata)) do
-              Phobos.logger.error do
-                { message: "error processing message, waiting #{interval}s" }.merge(error).merge(metadata)
-              end
-
-              sleep interval
-              metadata.merge!(retry_count: retry_count + 1)
+          instrument('listener.process_message', metadata) do |metadata|
+            time_elapsed = measure do
+              Phobos::Actions::ProcessMessage.new(
+                listener: @listener,
+                message: message,
+                listener_metadata: @listener_metadata,
+                encoding: @listener.encoding
+              ).execute
             end
 
-            raise Phobos::AbortError if @listener.should_stop?
-            retry
+            metadata.merge!(time_elapsed: time_elapsed)
           end
         end
       end
--- lib/phobos/actions/process_message.rb
+++ lib/phobos/actions/process_message.rb
@@ -1,18 +1,53 @@
 module Phobos
   module Actions
     class ProcessMessage
-      def initialize(listener:, message:, metadata:, encoding:)
+      include Phobos::Instrumentation
+
+      def initialize(listener:, message:, listener_metadata:, encoding:)
         @listener = listener
         @message = message
-        @metadata = metadata
+        @listener_metadata = listener_metadata
         @encoding = encoding
       end
 
       def execute
+        metadata = @listener_metadata.merge(
+          key: @message.key,
+          partition: @message.partition,
+          offset: @message.offset,
+          retry_count: 0
+        )
+
+        backoff = @listener.create_exponential_backoff
         payload = force_encoding(@message.value)
-        decoded_payload = @listener.handler_class.new.before_consume(payload)
-        @listener.handler_class.around_consume(decoded_payload, @metadata) do
-          @listener.handler_class.new.consume(decoded_payload, @metadata)
+
+        begin
+          decoded_payload = @listener.handler_class.new.before_consume(payload)
+          @listener.handler_class.around_consume(decoded_payload, metadata) do
+            @listener.handler_class.new.consume(decoded_payload, metadata)
+          end
+        rescue => e
+          retry_count = metadata[:retry_count]
+          interval = backoff.interval_at(retry_count).round(2)
+
+          error = {
+            waiting_time: interval,
+            exception_class: e.class.name,
+            exception_message: e.message,
+            backtrace: e.backtrace
+          }
+
+          instrument('listener.retry_handler_error', error.merge(metadata)) do
+            Phobos.logger.error do
+              { message: "error processing message, waiting #{interval}s" }.merge(error).merge(metadata)
+            end
+
+            sleep interval
+            metadata.merge!(retry_count: retry_count + 1)
+          end
+
+          raise Phobos::AbortError if @listener.should_stop?
+          retry
         end
       end
 
