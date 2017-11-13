61653919e7236d4454aef11609e682346dba366a
Mathias Klippinge <mathias.klippinge@gmail.com>
2017-11-13 13:11:56 +0100

    Add Travis integration
    - Run tests in docker (and locally)
    - Remove comments from spec helper
    - Pin ruby kafka to be lower than v0.5.0.
    - Do not send Gemfile.lock to docker container,
    as we may get a different version of installed gems compared to CI
    - Remove Circle CI integration
    - Update changelog to follow the http://keepachangelog.com/en/1.0.0/ format

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,49 +4,64 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## 1.5.0 (2017-10-25)
+## UNRELEASED
+### Added
+- Support for outputting logs as json #50
+- Make configuration, especially of listeners, more flexible. #31
 
-- [feature] Add `before_consume` callback to support single point of decoding a message klarna/phobos_db_checkpoint#34
-- [feature] Add module `Phobos::Test::Helper` for testing, to test consumers with minimal setup required
-- [feature] Allow configuration of backoff per listener #35
-- [changed] Move container orchestration into docker-compose
-- [changed] Update docker images #38
-- [bugfix] Make specs run locally #36
+### Changed
+- Pin ruby-kafka version to < 0.5.0 #48
+- Update changelog to follow the [Keep a Changelog](http://keepachangelog.com/) structure
 
-## 1.4.2 (2017-09-29)
+## [1.5.0] - 2017-10-25
+### Added
+- Add `before_consume` callback to support single point of decoding a message klarna/phobos_db_checkpoint#34
+- Add `Phobos::Test::Helper` for testing, to test consumers with minimal setup required
 
-- [bugfix] Async publishing always delivers messages #33
+### Changed
+- Allow configuration of backoff per listener #35
+- Move container orchestration into docker-compose
+- Update docker images #38
 
-## 1.4.1 (2017-08-22)
+### Fixed
+- Make specs run locally #36
 
-- [enhancement] Update dev dependencies to fix warnings for the new unified Integer class
-- [bugfix] Include the error `Kafka::ProcessingError` into the abort block
+## [1.4.2] - 2017-09-29
+### Fixed
+- Async publishing always delivers messages #33
 
-## 1.4.0 (2017-08-21)
+## [1.4.1] - 2017-08-22
+### Added
+- Update dev dependencies to fix warnings for the new unified Integer class
 
-- [enhancement] Add support for hash provided settings #30
+### Fixed
+- Include the error `Kafka::ProcessingError` into the abort block
 
-## 1.3.0 (2017-06-15)
+## [1.4.0] - 2017-08-21
+### Added
+- Support for hash provided settings #30
 
-- [enhancement] Add support for erb syntax in phobos config file #26
+## [1.3.0] - 2017-06-15
+### Added
+- Support for ERB syntax in Phobos config file #26
 
-## 1.2.1 (2016-10-12)
+## [1.2.1] - 2016-10-12
+### Fixed
+- Ensure JSON layout for log files
 
-- [bugfix] Ensure JSON layout for log files
+## [1.2.0] - 2016-10-10
+### Added
+- Log file can be disabled #20
+- Property (time_elapsed) available for notifications `listener.process_message` and `listener.process_batch` #24
+- Option to configure ruby-kafka logger #23
 
-## 1.2.0 (2016-10-10)
+## [1.1.0] - 2016-09-02
+### Added
+- Removed Hashie as a dependency #12
+- Allow configuring consumers min_bytes & max_wait_time #15
+- Allow configuring producers max_queue_size, delivery_threshold & delivery_interval #16
+- Allow configuring force_encoding for message payload #18
 
-- [enhancement] Log file can be disabled #20
-- [enhancement] Add a new extra (time_elapsed) for notifications "listener.process_message" and "listener.process_batch" #24
-- [enhancement] Add option to configure ruby-kafka logger #23
-
-## 1.1.0 (2016-09-02)
-
-- [enhancement] Removed Hashie as a dependency #12
-- [feature] Allow configuring consumers min_bytes & max_wait_time #15
-- [feature] Allow configuring producers max_queue_size, delivery_threshold & delivery_interval #16
-- [feature] Allow configuring force_encoding for message payload #18
-
-## 1.0.0 (2016-08-08)
-
-- Published on Github with full fledged consumers and producers
+## [1.0.0] - 2016-08-08
+### Added
+- Published on Github!
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,20 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 Thread.abort_on_exception = true
 
-# Set up coverage report
 require 'simplecov'
-# save to CircleCI's artifacts directory if we're on CircleCI
-if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
-  SimpleCov.coverage_dir(dir)
-end
-
 require 'coveralls'
+
 Coveralls::Output.silent = true
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ])
+
 SimpleCov.start do
   add_filter '/spec/'
   add_filter '/lib/phobos/test'
 end
-
 
 require 'phobos'
 require 'pry-byebug'
@@ -36,56 +30,21 @@ RSpec.configure do |config|
   include PhobosHelpers
   include CLIHelpers
 
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
   config.expect_with :rspec do |expectations|
-    # This option will default to `true` in RSpec 4. It makes the `description`
-    # and `failure_message` of custom matchers include text for helper methods
-    # defined using `chain`, e.g.:
-    #     be_bigger_than(2).and_smaller_than(4).description
-    #     # => "be bigger than 2 and smaller than 4"
-    # ...rather than:
-    #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-  # rspec-mocks config goes here. You can use an alternate test double
-  # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
-    # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will default to
-    # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
 
   config.run_all_when_everything_filtered = true
-
-  # Allows RSpec to persist some state between runs in order to support
-  # the `--only-failures` and `--next-failure` CLI options. We recommend
-  # you configure your source control system to ignore this file.
   config.example_status_persistence_file_path = 'spec/examples.txt'
-
-  # Limits the available syntax to the non-monkey patched syntax that is
-  # recommended. For more details, see:
-  #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
-  #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
-  #   - http://myronmars.to/n/dev-blog/2014/05/notable-changes-in-rspec-3#new__config_option_to_disable_rspeccore_monkey_patching
   config.disable_monkey_patching!
-
-  # This setting enables warnings. It's recommended, but in some cases may
-  # be too noisy due to issues in dependencies.
   config.warnings = false
-
   config.expose_dsl_globally = true
 
-  # Many RSpec users commonly either run the entire suite or an individual
-  # file, and it's useful to allow more verbose output when running an
-  # individual spec file.
   if config.files_to_run.one?
-    # Use the documentation formatter for detailed output,
-    # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
     config.default_formatter = 'doc'
   end
 
@@ -94,20 +53,9 @@ RSpec.configure do |config|
     Phobos.configure('config/phobos.yml.example')
   end
 
-  # Print the 10 slowest examples and example groups at the
-  # end of the spec run, to help surface which specs are running
-  # particularly slow.
   config.profile_examples = false
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
   config.order = :random
 
-  # Seed global randomization in this process using the `--seed` CLI option.
-  # Setting this allows you to use `--seed` to deterministically reproduce
-  # test failures related to randomization by passing the same `--seed` value
-  # as the one that triggered the failure.
   Kernel.srand config.seed
 end
