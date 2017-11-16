34b32d8f9bd613c0f77abc28312ab8094eba1ce8
Mathias Klippinge <mathias.klippinge@gmail.com>
2017-11-16 14:52:13 +0100

    Make metadata optional. Use real Kafka::FetchedMessage instead of struct. Update ProcessMessage API contract

--- lib/phobos/test/helper.rb
+++ lib/phobos/test/helper.rb
@@ -3,9 +3,8 @@ module Phobos
     module Helper
       Topic = 'test-topic'
       Group = 'test-group'
-      KafkaMessage = Struct.new(:value)
 
-      def process_message(handler:, payload:, metadata:, force_encoding: nil)
+      def process_message(handler:, payload:, metadata: {}, force_encoding: nil)
         listener = Phobos::Listener.new(
           handler: handler,
           group_id: Group,
@@ -13,11 +12,18 @@ module Phobos
           force_encoding: force_encoding
         )
 
+        message = Kafka::FetchedMessage.new(
+          value: payload,
+          key: nil,
+          topic: Topic,
+          partition: 0,
+          offset: 13,
+        )
+
         Phobos::Actions::ProcessMessage.new(
           listener: listener,
-          message: KafkaMessage.new(payload),
-          metadata: metadata,
-          encoding: listener.encoding
+          message: message,
+          listener_metadata: metadata
         ).execute
       end
     end
