3af84a8548a6d63354035efc1cae271fdfc88828
Mathias Klippinge <mathias.klippinge@gmail.com>
2018-05-07 13:15:32 +0200

    Upgrade to ruby 2.5.1 by default

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM ruby:2.4.1-alpine
+FROM ruby:2.5.1-alpine
 
 RUN apk update && apk upgrade && \
     apk add --no-cache bash git openssh build-base
-RUN gem install bundler -v 1.16.0
+RUN gem install bundler -v 1.16.1
 
 WORKDIR /opt/phobos
 
--- spec/lib/phobos/cli_spec.rb
+++ spec/lib/phobos/cli_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Phobos::CLI do
   describe '$ phobos' do
     it 'prints help text' do
       output = capture_stdout { Phobos::CLI::Commands.start([]) }
-      expect(output).to include 'help [COMMAND]  # Describe available commands or one specific command'
+      expect(output).to include 'help [COMMAND]  # Describe available commands or one...'
     end
   end
 
