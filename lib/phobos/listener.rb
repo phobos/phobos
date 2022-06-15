c9e82f69d27b3e5a045a1589b195230e5d0745f5
Daniel Orner <daniel.orner@wishabi.com>
2022-06-15 15:10:10 -0400

    CR fix

--- lib/phobos/listener.rb
+++ lib/phobos/listener.rb
@@ -42,14 +42,11 @@ module Phobos
 
     def start
       @signal_to_stop = false
-      error = nil
-
       start_listener
 
       begin
         start_consumer_loop
       rescue Kafka::ProcessingError, Phobos::AbortError => e
-        error = e if e.is_a?(Kafka::ProcessingError)
         # Abort is an exception to prevent the consumer from committing the offset.
         # Since "listener" had a message being retried while "stop" was called
         # it's wise to not commit the batch offset to avoid data loss. This will
@@ -57,10 +54,10 @@ module Phobos
         instrument('listener.retry_aborted', listener_metadata) do
           log_info('Retry loop aborted, listener is shutting down', listener_metadata)
         end
+        raise e if e.is_a?(Kafka::ProcessingError)
       end
     ensure
       stop_listener
-      raise error if error
     end
 
     def stop
